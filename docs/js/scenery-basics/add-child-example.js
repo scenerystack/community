b0f2e8c80cfcc6baa4bdc6deb3e5fd956b555f62
Jonathan Olson <jonathan.olson@colorado.edu>
2025-01-29 10:37:14 -0700

    add-child-example improvement

--- docs/js/scenery-basics/add-child-example.js
+++ docs/js/scenery-basics/add-child-example.js
@@ -4,8 +4,9 @@ createSandbox( "add-child-example", ( scene, stepEmitter, display ) => {
   const box = ( () => {
     /*START*/
     const content = new Node();
-    for ( let i = 0; i < 5; i++ ) {
-      content.addChild( new Rectangle( i * 50, 0, 50, 50, {
+    for ( let i = 0; i < 7; i++ ) {
+      content.addChild( new Circle( 30, {
+        x: i * 40,
         fill: `rgb(${i * 40},0,0)`
       } ) );
     }
